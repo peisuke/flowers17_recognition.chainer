235b457e3ccb91df046b4e2db521266d7a13c754
peisuke <peisuke.com@gmail.com>
2019-04-23 19:26:14 +0900

    fix

--- preparation.py
+++ preparation.py
@@ -6,6 +6,6 @@ os.makedirs('images', exist_ok=True)
 files = glob.glob('jpg/*.jpg')
 for f in files:
     img = Image.open(f)
-    img = img.resize((224, 224))
+    img = img.resize((64, 64))
     target = os.path.join('images', os.path.basename(f))
     img.save(target)
